d5e1397347bb9b0fdae534e52e9446efb3ae9f1b
MrClue <giselle.evita@gmail.com>
2026-06-11 13:47:20 +0200

    Remove missing rating configuration import

--- backend/rating-service/src/main/kotlin/com/evidentia/rating/RatingServiceApplication.kt
+++ backend/rating-service/src/main/kotlin/com/evidentia/rating/RatingServiceApplication.kt
@@ -1,12 +1,9 @@
 package com.evidentia.rating
 
-import com.evidentia.common.config.CommonConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(CommonConfiguration::class)
 class RatingServiceApplication
 
 fun main(args: Array<String>) {
